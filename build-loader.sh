e8ef9345e03a008ef43be4bcff43df82e9a05bc0
Jinho Yoon <37324479+ek2rlstk@users.noreply.github.com>
2021-10-31 17:58:51 +0900

    Update build-loader.sh

--- build-loader.sh
+++ build-loader.sh
@@ -206,8 +206,9 @@ if [ ! -d "${BRP_UPAT_DIR}" ]; then
   brp_verify_file_sha256 "${BRP_PAT_FILE}" "$(brp_json_get_field "${BRP_REL_CONFIG_JSON}" "os.sha256")"
   brp_unpack_tar "${BRP_PAT_FILE}" "${BRP_UPAT_DIR}"
   if [ ! -f "${BRP_UPAT_DIR}/zImage" ]; then
-    ar x "$(ls ${BRP_UPAT_DIR}/flashupdate*)"
-    tar xf "${BRP_UPAT_DIR}/data.tar.xz"
+    cd ${BRP_UPAT_DIR}
+    ar x "$(ls flashupdate*)"
+    tar xf "data.tar.xz"
   fi
 
 else
